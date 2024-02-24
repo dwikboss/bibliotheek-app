ba3d6c40c126ded646c2c9c7f71132d60a21d2f0
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-02-24 17:30:58 +0100

    add: add timestamps at comments

--- frontend/src/components/CommentSlider.vue
+++ frontend/src/components/CommentSlider.vue
@@ -1,13 +1,24 @@
 <template>
-    <swiper :slidesPerView="3" :spaceBetween="25" :loop="true" :freeMode="true" :modules="modules" class="mySwiper" :autoplay="{
-        delay: 1500,
-        disableOnInteraction: false,
-    }">
-        <swiper-slide v-for="comment in comments" :key="comment.id">
-            <p class="comment">{{ comment.comment }}</p>
-            <p class="name">{{ comment.name }}</p>
-        </swiper-slide>
-    </swiper>
+  <swiper
+    :slidesPerView="3"
+    :spaceBetween="25"
+    :loop="true"
+    :freeMode="true"
+    :modules="modules"
+    class="mySwiper"
+    :autoplay="{
+      delay: 1500,
+      disableOnInteraction: false,
+    }"
+  >
+    <swiper-slide v-for="comment in comments" :key="comment.id">
+      <p class="comment">{{ comment.comment }}</p>
+      <div class="comment-info">
+        <p class="name">{{ comment.name }}</p>
+        <p class="time">{{ formatTimestamp(comment.created_at) }}</p>
+      </div>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script lang="ts">
@@ -21,54 +32,79 @@ import 'swiper/css/pagination';
 import { Autoplay } from 'swiper/modules';
 
 export default defineComponent({
-    name: 'CommentSlider',
-    data() {
-        return {
-            modules: [Autoplay],
-        };
+  name: 'CommentSlider',
+  data() {
+    return {
+      modules: [Autoplay],
+    };
+  },
+  props: {
+    comments: {
+      type: Array as () => _IComment[],
+      default: () => [],
     },
-    props: {
-        comments: {
-            type: Array as () => _IComment[],
-            default: () => []
-        }
+  },
+  methods: {
+    formatTimestamp(timestamp: any) {
+      const now = new Date();
+      const created = new Date(timestamp);
+      const diff = now.getTime() - created.getTime();
+
+      if (diff < 10 * 60 * 1000) {
+        return 'Just now';
+      }
+
+      const hours = String(created.getHours()).padStart(2, '0');
+      const minutes = String(created.getMinutes()).padStart(2, '0');
+      return `${hours}:${minutes}`;
     },
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
 });
 </script>
 
 <style lang="scss">
 .swiper {
-    width: 100%;
-    height: 25vh;
+  width: 100%;
+  height: 25vh;
 }
 
 .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #ffffff;
-    border-radius: 10px;
-    padding: 25px;
+  text-align: center;
+  font-size: 18px;
+  background: #ffffff;
+  border-radius: 10px;
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  border: 1px solid #ff732045;
+
+  .comment-info {
     display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    border: 1px solid #ff732045;
+    justify-content: space-between;
 
-    p.comment {
-        font-family: 'Rijksoverheid Bold';
-        color: var(--blue);
-        text-align: left;
-        font-size: 1.2rem;
-        margin-bottom: 15px;
+    .time {
+        font-family: 'Rijksoverheid Regular';
+        color: #00000067;
     }
+  }
 
-    p.name {
-        font-family: 'Rijksoverheid Serif Italic';
-        color: var(--orange);
-        text-align: left;
-    }
+  p.comment {
+    font-family: 'Rijksoverheid Bold';
+    color: var(--blue);
+    text-align: left;
+    font-size: 1.2rem;
+    margin-bottom: 15px;
+  }
+
+  p.name {
+    font-family: 'Rijksoverheid Serif Italic';
+    color: var(--orange);
+    text-align: left;
+  }
 }
 </style>
