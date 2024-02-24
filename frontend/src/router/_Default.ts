b1ac70ab0b62e2fa7ba2d5b22d882a15a870f499
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-02-24 16:06:08 +0100

    mobile form

--- frontend/src/router/_Default.ts
+++ frontend/src/router/_Default.ts
@@ -1,12 +1,18 @@
 import { PageName } from '@/utils/_Constants';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Mobile from '../views/Mobile.vue';
 
 const routes = [
   {
     path: '/',
     name: PageName.HOME,
     component: Home,
+  },
+  {
+    path: '/mobile',
+    name: PageName.MOBILE,
+    component: Mobile,
   },
 ];
 
