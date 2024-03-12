536994b8cae1c1fc70205b7bc86411108bc3d86a
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-03-12 23:53:21 +0100

    fix

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -12,7 +12,7 @@
     <div class="qr-section">
       <img src="@/assets/images/qr-code-new.png" alt="qr" />
       <div class="qr-text">
-        <p>Ook een reactie achterlaten? Scan de QR code!</p>
+        <p>Ook een reactie achterlaten? Scan de QR code</p>
         <p>Of ga naar https://statement-otd.vercel.app/mobile</p>
       </div>
     </div>
